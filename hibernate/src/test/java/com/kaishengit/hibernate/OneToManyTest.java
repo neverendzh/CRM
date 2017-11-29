e81bd49319bff91bf535aec65d261ba20f98d018
zhangchengjun <neverend@163.com>
2017-11-29 16:22:13 +0800

    Hibernate的一对多的关系增加和删除的使用

--- hibernate/src/test/java/com/kaishengit/hibernate/OneToManyTest.java
+++ hibernate/src/test/java/com/kaishengit/hibernate/OneToManyTest.java
@@ -10,11 +10,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 /**
- * 央射关系
+ * 央射关系 一对多保存与删除
  * Created by Administrator on 2017/11/28.
  */
 public class OneToManyTest {
@@ -48,6 +49,9 @@ public class OneToManyTest {
         System.out.println(user.getName()+"-->"+user.getId());
     }
 
+    /**
+     * 查询出一个User中对应的多个地址
+     */
     @Test
     public void findUser(){
         User user = (User) session.get(User.class,1);
@@ -72,4 +76,87 @@ public class OneToManyTest {
         }
     }
 
+    /**
+     * 保存用户
+     */
+    @Test
+    public void saveUser(){
+        User user = new User();
+        user.setName("小王");
+        session.saveOrUpdate(user);
+    }
+
+    /**
+     * 多对一的添加收货地址
+     */
+    @Test
+    public void saveAddress(){
+        User user = (User) session.get(User.class,4);
+
+        Address address = new Address();
+        address.setCityName("上海");
+        address.setAddress("浦东区");
+
+        address.setUser(user);
+
+        session.save(address);
+    }
+
+
+    /**
+     * 一个人对应对个收货地址的保存
+     */
+    @Test
+    public void save(){
+
+        User user = new User();
+        user.setName("tom");
+
+        Address address = new Address();
+        address.setCityName("纽约");
+        address.setAddress("莫宁路110号");
+        address.setUser(user);
+
+        Address address1 = new Address();
+        address1.setCityName("南京");
+        address1.setAddress("小胡同");
+        address1.setUser(user);
+
+//        包Address放入set集合中在传给User对象这样就可以使User维护关联关系，
+//        如果在User.hbm.xml中配置inverse="true"则不会维护关联关系，这样写了也没用。
+      /*  Set<Address> addressSet = new HashSet<Address>();
+        addressSet.add(address);
+        addressSet.add(address1);
+        user.setAddressSet(addressSet);*/
+
+//      最佳实践，在保存这种一对多的数据时，应该先存一在存多。只需要让多的一端去维护关系
+        session.save(user);
+        session.save(address);
+        session.save(address1);
+    }
+
+
+    /**
+     *删除有外键约束的的数据
+     *Address的外键user_id是User表中id的值，先删除User对应在Address表中的收获地址。
+     *再删除User用户
+     */
+    @Test
+    public void delete(){
+        Criteria criteria = session.createCriteria(Address.class);
+        criteria.add(Restrictions.eq("user.id",11));
+        List<Address> addressList = criteria.list();
+        for(Address address : addressList){
+            session.delete(address);
+        }
+
+        User user = (User) session.get(User.class,11);
+        session.delete(user);
+    }
+
+    @Test
+    public void delete2(){
+        User user = (User) session.get(User.class,12);
+        session.delete(user);
+    }
 }
